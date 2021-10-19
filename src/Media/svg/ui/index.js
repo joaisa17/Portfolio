3206536a51f434480e882bf749552aa9777a4f84
joaisa17 <joaisa17@innlandetfylke.no>
2021-10-19 13:12:36 +0200

    affaafsaf

---
+++ src/Media/svg/ui/index.js
@@ -0,0 +1,15 @@
+import AudioMuted from './AudioMuted.svg';
+import AudioUnmuted from './AudioUnmuted.svg';
+import PauseButton from './PauseButton.svg';
+import PlayButton from './PlayButton.svg';
+import RepeatButton from './RepeatButton';
+import FullscreenButton from './FullscreenButton.svg';
+
+export {
+    AudioMuted,
+    AudioUnmuted,
+    PauseButton,
+    PlayButton,
+    RepeatButton,
+    FullscreenButton
+}
